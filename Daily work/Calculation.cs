0cb99d187d0989cfa393ce8514f9e02f519d48f7
NandiniShivaiah <Nandini.Shivaiah@ust.com>
2023-10-17 17:29:48 +0530

    first commit

---
+++ Daily work/Calculation.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices.Marshalling;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace basic_program
+{
+    internal class Calculation
+    {
+        double num1, num2, ans;
+        public void add(int num1, int num2)
+        {
+            ans = num1 + num2;
+            Console.WriteLine(ans);
+
+        }
+    }
+}
