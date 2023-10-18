34e0c817bb64750b29fd039718b4de8e8f63668e
NandiniShivaiah <Nandini.Shivaiah@ust.com>
2023-10-18 18:03:19 +0530

    first commit

---
+++ Assignment1/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+/*Console.WriteLine("Hello, World!");*/
+
+using Assignment;
+
+Student student = new Student("Nandini",50,60,70);
+
+Console.WriteLine(student.CalculateAverage());
+
+
+
+
+
---
+++ Assignment1/Student.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment
+{
+     class Student
+    {
+
+        private string? studentname;
+        private int grades1, grades2, grades3, grades4,grades5;
+
+        public Student(string? studentname, int grades1, int grades2, int grades3 )
+        {
+            this.studentname = studentname;
+            this.grades1 = grades1;
+            this.grades2 = grades2;
+            this.grades3 = grades3;
+            
+        }
+
+        public double CalculateAverage()
+        {
+            double average = (grades1 + grades2 + grades3 ) / 3;
+            return average;
+        }
+    }
+
+}
