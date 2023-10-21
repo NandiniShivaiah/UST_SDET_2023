e5e63497c1b58b4957b152c8f5fb43c87de932ef
NandiniShivaiah <Nandini.Shivaiah@ust.com>
2023-10-21 15:42:37 +0530

    first commit

---
+++ 20-10-23/Product.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment1
+{
+    internal class Product
+
+
+    {
+
+
+        public Product(String? ProductName, double price, int quantity)
+        {
+            ProductName = ProductName;
+            price = price;
+            quantity = quantity;
+        }
+
+        public string? ProductName { get; set; }
+        public double Price { get; set; }
+        public int Quantity { get; set; }
+
+        public void SetPrice(double newPrice)
+        {
+            Price = newPrice;
+        }
+        public double ProductValue()
+        {
+            return Price * Quantity;
+        }
+        public void DisplayProduct()
+        {
+            Console.WriteLine("Product Name:" + ProductName + "Price:" + Price + "Quantity:" + Quantity");
+        }
+    }
+}
+
+        /*private string? productName;
+        private double price,quantity;
+
+        public string? ProductName { get => productName; set => productName = value; }
+        public double Price { get => price; set => price = value; }
+        public double Quantity { get => quantity; set => quantity = value; }
+
+        public Product(string? productName, double price, double quantity)
+        {
+            this.ProductName = productName;
+            this.Price = price;
+            this.Quantity = quantity;
+        }
+
+        public void setPrice(double newPrice)
+        {
+            Price = newPrice;
+        }
+        public double ProductValue()
+        {
+            return Price*Quantity;
+        }
+        public void DisplayProductDetails()
+        {
+            Console.WriteLine("ProductName:{productName}");
+            Console.WriteLine("Price:{price}");
+            Console.WriteLine("Quantity:{quantity}");
+
+        }
+    }
+}
---
+++ 20-10-23/Program.cs
@@ -0,0 +1,69 @@
+﻿// See https://aka.ms/new-console-template for more information
+/*Console.WriteLine("Hello, World!");*/
+
+using Assignment1;
+
+
+ElectronicsProduct electronicsProduct = new(3, "mobile", 30000, 1);
+electronicsProduct.DisplayProduct();
+electronicsProduct.ElectronicsProductDisplay();
+
+DigitalProduct digitalProduct = new(3, "Camera", 20000, 4, "MP4");
+digitalProduct.DisplayProduct();
+digitalProduct.ElectronicsProductDisplay();
+
+Console.WriteLine("Choose");
+Console.WriteLine("1->DigitalProduct\n 2->ElectronicsProduct\n" + "3->ClothingProduct");
+switch(Convert.ToInt32(Console.ReadLine()))
+{
+    case 1:
+        DigitalProduct digitalProduct = new(3, "Camera", 20000, 4, "MP4");
+        digitalProduct.DisplayProduct();
+        digitalProduct.ElectronicsProductDisplay();
+        digitalProduct.DisplayDigitalProduct();
+        break;
+    case 2:
+        ElectronicsProduct electronicsProduct = new(3, "mobile", 30000, 1);
+        electronicsProduct.DisplayProduct();
+        electronicsProduct.ElectronicsProductDisplay();
+        break;
+    case 3:
+        ClothingProduct clothingProduct = new("Dress", 3000, 3, "s");
+        ClothingProduct.DisplayProduct();
+        ClothingProduct.DisplayClothing();
+        break;
+    default:Console.WriteLine("Please check the numberyou have entered");
+        break;
+
+
+
+
+
+}
+
+
+/*Student[] student = new Student[3];
+student[0] = new Student("NAM", new int[] { 20, 30, 10 }, "A");
+student[1] = new Student("SAM", new int[] { 10, 10, 10 }, "B");
+student[2] = new Student("GAM", new int[] { 11, 9, 3 }, "C");
+
+foreach(Student s in student)
+{
+    s.DisplayStudentInfo();
+    s.GetMarksSummary();
+}
+
+/*Product product1 = new Product("aaa", 30, 2);
+Product product2 = new Product("xxx", 10, 3);
+Product product3 = new Product("aaa", 45, 4);
+
+Product[] products = { product1, product2, product3 };
+
+for(int i=0;i<products.Length;i++)
+{
+    Console.WriteLine("Product Name:" + products[i].ProductName);
+    Console.WriteLine("Price:" + products[i].Price);
+    Console.WriteLine("Quantity:" + products[i].Quantity);
+    Console.WriteLine("Total Value:" + products[i].ProductValue());
+
+}*/
