25f96db03047c3d417d58e41f14fc75ff7975c31
NandiniShivaiah <Nandini.Shivaiah@ust.com>
2023-10-23 19:30:13 +0530

    first commit

---
+++ 23-10-23/Employee.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment1
+{
+    internal class Employee:Person,IDisplayable
+    {
+        public int EmployeeId { get; set; }
+        public Employee(string? firstName, string? lastName, int age,int employeeId) : base(firstName, lastName, age)
+        {
+            FirstName = firstName;
+            LastName = lastName;    
+            Age = age;
+            EmployeeId = employeeId;
+        }
+
+        
+      
+
+        
+
+       
+
+        public void DisplayInfo()
+        {
+            Console.WriteLine("Firstname:" + FirstName + "Lastname:" + LastName + "Age:" +
+                Age + "employeeId:" + EmployeeId );
+        }
+
+      
+    }
+}
---
+++ 23-10-23/Person.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment1
+{
+    internal class Person
+    {
+        private string? firstName;
+        private string? lastName;
+        private int age;
+
+        public Person(string? firstName, string? lastName, int age)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Age = age;
+        }
+
+        public string? FirstName { get; set; }
+        public string? LastName  { get; set; }
+        public int Age
+        { get =>age; 
+            set
+            {
+                if(value>=18 && value<=100)
+                {
+                    age = value;
+                }
+            }
+        }
+
+       
+        
+
+    }
+}
