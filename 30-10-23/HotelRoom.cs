eb19d19fd3e7e033cf3e73fd4895234f198f4234
NandiniShivaiah <Nandini.Shivaiah@ust.com>
2023-10-31 19:33:38 +0530

    first commit

---
+++ 30-10-23/HotelRoom.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment1
+{
+    internal class HotelRoom
+    {
+        public int RoomNumber;
+        public string? RoomType;
+        public bool IsBooked;
+    }
+}
+
+
+       /* public HotelRoom(int RoomNumber )
+        {
+            this.RoomNumber = RoomNumber;
+        }
+
+        public HotelRoom(string RoomType)
+        {
+            this.RoomType = RoomType;
+        }
+
+        public HotelRoom(bool IsBooked)
+        {
+            this.IsBooked = IsBooked;
+        }
+    }
+}
+
+       
+
+       /* public HotelRoom(int roomNumber, string? roomType, bool iSBooked)
+        {
+            RoomNumber = roomNumber;
+            RoomType = roomType;
+            ISBooked = iSBooked;
+        }
+
+        public int  RoomNumber { get; set; }
+        public string? RoomType { get; set; }
+
+        public bool ISBooked { get; set; }
+        public int RoomNumber1 { get => this.RoomNumber; set => this.RoomNumber = value; }
+    }
+}*/
+
---
+++ 30-10-23/RoomReservation.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment1
+{
+    internal class RoomReservation<T> : HotelRoom
+    {
+
+        public void Booking<T>(int roomNum)
+        {
+            if (RoomNumber == roomNum)
+            {
+                IsBooked = true;
+                Console.WriteLine("Booking confirmed");
+            }
+            else
+            {
+                Console.WriteLine("Booking not available" +
+                    " with this Room number");
+            }
+
+
+
+        }
+
+        public void Cancelation<T>(int roomNum)
+        {
+            if (RoomNumber == roomNum && IsBooked == true)
+            {
+
+                Console.WriteLine("Booking Canceled");
+            }
+            else
+            {
+                Console.WriteLine("Cannot cancel because " +
+                    " no booking available with" +
+                    " this Room number");
+            }
+
+        }
+    }
+}
+       /*
+       public void BookRoom( RoomNumber room )
+        {
+            if(room != null && !room.ISBooked)
+            {
+                room.ISBooked = true;
+                Console.WriteLine("Room has been booked");
+            }
+            else
+            {
+                Console.WriteLine("Room not available/ already booked");
+            }
+            
+        }
+
+        public void CancelReservation(RoomNumber room)
+        {
+            if (room != null && !room.ISBooked)
+            {
+                room.ISBooked = false;
+                Console.WriteLine("Cancel reservation");
+            }
+            else
+            {
+                Console.WriteLine("Room  already booked");
+            }
+           
+        }
+
+           
+            
+
+        }
+        
+    }
+}
+*/
