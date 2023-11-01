fb0f7d6c01c355a2cf7cc97b709392283ae37560
NandiniShivaiah <Nandini.Shivaiah@ust.com>
2023-11-01 19:18:48 +0530

    first commit

---
+++ 01-11-23/TourismDestination.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment1
+{
+    internal class TourismDestination
+    {
+        public TourismDestination(string? name, string? country, int rate)
+        {
+            Name = name;
+            Country = country;
+            Rate = rate;
+        }
+
+        public string? Name { get; set; }
+        public string? Country { get; set; }
+        public int Rate { get; set; }
+
+
+
+        public static void SortOnRating(List<TourismDestination> list)
+        {
+            if (list != null)
+            {
+                var rate = list.OrderByDescending(
+                a => a.Rate);
+                foreach (var l in rate)
+                {
+                    Console.WriteLine("Destination name: " + l.Name +
+                        " Country: " + l.Country + " Rating: " + l.Rate);
+                }
+            }
+            else
+            {
+                Console.WriteLine("No destination added");
+            }
+
+        }
+    }
+}
